6e2d4d78d35045d7c00085953287c5155a5d8c3d
ErickSLuna <ericksluna53@gmail.com>
2025-11-13 17:51:02 -0700

    adding more team members

--- resources/views/components/AboutUs/about.blade.php
+++ resources/views/components/AboutUs/about.blade.php
@@ -51,6 +51,51 @@ UACJ Cd. Juárez, Chihuahua</p>",
 ];
 $contentJulio = "<p> Over 25 years of experience</p>";
 
+$contentAdriana = "<p>  Graduated from the Autonomous University of Ciudad Juárez, 
+Chihuahua and Diploma In Implantology</p>";
+$sectionsAdriana = [
+    [
+                'title' => ' Training',
+        'content' => "<ul class='list-disc pl-5 space-y-0.5'><li class='text-gray-600 text-[15px] leading-snug'>Basic dental implant training May 1998</li>
+<li class='text-gray-600 text-[15px] leading-snug'> Scientific conferences in endodontics May 1999</li>
+<li class='text-gray-600 text-[15px] leading-snug'>National and international congress March 1999</li>
+<li class='text-gray-600 text-[15px] leading-snug'>National meeting of the Mexican association of endodontics 
+September 2000</li>
+<li class='text-gray-600 text-[15px] leading-snug'>Dental seminar adhesive restorations: invisible dentistry 
+January 2006</li>
+<li class='text-gray-600 text-[15px] leading-snug'>Dental seminar adhesive restorations: invisible dentistry 
+hands-on workshop January 2006</li>
+<li class='text-gray-600 text-[15px] leading-snug'>Dentistry Congress August 2012</li>
+<li class='text-gray-600 text-[15px] leading-snug'>Diploma in aesthetic and restorative dentistry April 2016 to 
+January 2017</li>
+<li class='text-gray-600 text-[15px] leading-snug'>Veneers course October 2018</li>
+<li class='text-gray-600 text-[15px] leading-snug'>Smile Design Course & veneers November 2018</li>
+<li class='text-gray-600 text-[15px] leading-snug'>AMODYB International Congress October 2018</li></ul>",
+    ]
+];
+
+$sectionsMario = [
+    [
+        'title' => ' Professional Associations',
+        'content' => "<ul class='list-disc pl-5 space-y-0.5'><li class='text-gray-600 text-[15px] leading-snug'> Miembro del Colegio de Cirujanos Dentistas de Cd. Juárez, 
+Chih.</li>
+<li class='text-gray-600 text-[15px] leading-snug'> Miembro de la Asociación Estatal A.D.M. Chih.</li>
+<li class='text-gray-600 text-[15px] leading-snug'> Socio del Clúster de Salud y Turismo Médico de Cd Juárez</li>
+<li class='text-gray-600 text-[15px] leading-snug'> Actividad profesional</li>
+<li class='text-gray-600 text-[15px] leading-snug'> Práctica general desde 2004 en Cd. Juárez, Chih.</li>
+<li class='text-gray-600 text-[15px] leading-snug'> Practica en Endodoncia desde 2008</li>
+<li class='text-gray-600 text-[15px] leading-snug'> Curso: Solución de problemas en Endodoncia y Manejo de 
+Tomografía en la Endodoncia          
+Julio 2022</li>
+<li class='text-gray-600 text-[15px] leading-snug'> Curso de Rehabilitación Sobre Implantes      junio 2022</li>
+<li class='text-gray-600 text-[15px] leading-snug'> Diplomado Básico de Implantología Bucal y Maxilofacial A.C. 
+septiembre 2021 febrero 2022</li>
+<li class='text-gray-600 text-[15px] leading-snug'> Constancia Dental de Provincia Mayo 2022  Monterrey N. l.</li>
+<li class='text-gray-600 text-[15px] leading-snug'> Constancia Cloudcroft  New.Mex Septiembre 2022</li>
+<li class='text-gray-600 text-[15px] leading-snug'> Constancia Congreso ADM AMIC Noviembre 2022</li></ul>",
+    ]
+];
+
 @endphp
 
 <x-doctor-profile 
@@ -70,8 +115,31 @@ $contentJulio = "<p> Over 25 years of experience</p>";
 
 <x-doctor-profile 
     :imageUrl="asset('images/IMG_100183.jpg')"
-    name="Dr. Julio Nevárez"
-    :specialties="['Prosthodontist', 'Cosmetic Dentist', 'Implantologist']"
+    name=" Dra. Adriana Armendáriz"
+    :specialties="['General Dentist']"
     :content="$contentJulio"
-    :sections="$sectionsJulio"
+    :sections="$sectionsAdriana"
+/>
+
+<x-doctor-profile 
+    :imageUrl="asset('images/IMG_100198.jpg')"
+    name=" Dra. Adriana Escalante"
+    :specialties="['General Dentist']"
+    {{-- :content="$contentAdriana"
+    :sections="$sectionsAdriana" --}}
+/>
+
+<x-doctor-profile 
+    :imageUrl="asset('images/IMG_100191.jpg')"
+    name=" Dr. Mario Torres"
+    :specialties="['Endodontist']"
+    :sections="$sectionsMario"
+/>
+
+<x-doctor-profile 
+    :imageUrl="asset('images/IMG_100184.jpg')"
+    name="Dra. Liliana Estrada"
+    :specialties="['Pediatric Dentist']"
+    {{-- :content="$contentAdriana"
+    :sections="$sectionsAdriana" --}}
 />
