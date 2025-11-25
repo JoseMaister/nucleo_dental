2f1611e27ca06744ba66c63216b704b798a1a45d
ErickSLuna <ericksluna53@gmail.com>
2025-11-25 16:57:25 -0700

    hotfix/fix-responsive-home-hero

--- resources/views/components/sections/hero.blade.php
+++ resources/views/components/sections/hero.blade.php
@@ -3,8 +3,8 @@
          class="w-full h-[500px] object-cover" 
          alt="Dental Clinic">
     <div class="absolute inset-0 bg-black bg-opacity-30"></div>
-    <div class="absolute top-3/4 left-6 md:left-10 transform -translate-y-1/2 text-white max-w-xl">
-        <h2 class="text-4xl font-bold leading-tight">{!! __('messages.hero.title') !!}</h2>
+    <div class="absolute top-[50px] left-6 md:left-10 transform  text-white max-w-xl">
+        <h2 class="text-2xl md:text-4xl font-bold leading-tight">{!! __('messages.hero.title') !!}</h2>
         <p class="mt-4 text-lg">{!! __('messages.hero.subtitle') !!}</p>
         <a href="{{ route('services') }}" class="mt-6 inline-block bg-indigo-700 hover:bg-indigo-800 text-white px-6 py-3 rounded-full shadow-lg">
             {{ __('messages.hero.cta') }}
